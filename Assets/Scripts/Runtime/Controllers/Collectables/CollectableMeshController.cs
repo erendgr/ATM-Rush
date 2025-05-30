ac8f885de3d1d95083274e73d95b03eb1f9e4a2b
erendgr <49555544+erendgr@users.noreply.github.com>
2025-05-30 17:20:44 +0300

    Collectable System Finalized

---
+++ Assets/Scripts/Runtime/Controllers/Collectables/CollectableMeshController.cs
@@ -0,0 +1,45 @@
+using Runtime.Data.ValueObject;
+using UnityEngine;
+
+namespace Runtime.Controllers.Collectables
+{
+    public class CollectableMeshController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private MeshFilter meshFilter;
+
+        #endregion
+
+        #region Private Variables
+
+        private CollectableMeshData _data;
+
+        #endregion
+
+        #endregion
+
+
+        private void OnEnable()
+        {
+            ActivateMeshVisuals();
+        }
+
+        internal void SetMeshData(CollectableMeshData meshData)
+        {
+            _data = meshData;
+        }
+
+        private void ActivateMeshVisuals()
+        {
+            meshFilter.mesh = _data.MeshList[0];
+        }
+
+        internal void UpgradeCollectableVisual(int value)
+        {
+            meshFilter.mesh = _data.MeshList[value];
+        }
+    }
+}
