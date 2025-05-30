92d57ddc37c3a44317433271d4924357da2e15a9
erendgr <49555544+erendgr@users.noreply.github.com>
2025-05-30 15:31:25 +0300

    Object Pooling Module Update

--- Assets/_Modules/ObjectPooling/Scripts/Data/ValueObjects/PoolData.cs
+++ Assets/_Modules/ObjectPooling/Scripts/Data/ValueObjects/PoolData.cs
@@ -1,12 +1,10 @@
 using System;
 using _Modules.ObjectPooling.Scripts.Enums;
-using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace _Modules.ObjectPooling.Scripts.Data.ValueObjects
 {
     [Serializable]
-    [HideReferenceObjectPicker]
     public class PoolData
     {
         public int Amount;
